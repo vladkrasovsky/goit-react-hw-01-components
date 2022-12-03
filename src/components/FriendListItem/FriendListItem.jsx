4abe31bc9e1eb47936396cc23d5398e96a8cd285
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-03 11:05:49 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
-console.log(css);
-
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
